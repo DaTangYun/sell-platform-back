01d7effe62a1212a996a9ff812279f1424dc8ac4
z790490122 <790490122@qq.com>
2019-01-21 12:00:32 +0800

    ''

---
+++ application/api/controller/Config.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\api\controller;
+
+use app\common\controller\Api;
+
+/**
+ * 网站配置接口
+ */
+class Config extends Api
+{
+
+    protected $noNeedLogin = ['*'];
+    protected $noNeedRight = ['*'];
+
+    /**
+     * 头部配置
+     * 
+     */
+    public function header()
+    {
+    	if($this->request->isGet()){
+    		$this->success('请求成功');
+    	}
+    }
+
+    /**
+     * 底部配置
+     * 
+     */
+    public function footer()
+    {
+        if($this->request->isGet()){
+            $this->success('请求成功');
+        }
+    }
+
+
+    /**
+     * 登录页配置
+     * 
+     */
+    public function login()
+    {
+        if($this->request->isGet()){
+            $this->success('请求成功');
+        }
+    }
+}
