6fabf3653fdc499f7b9b4865c074955b4e294395
z790490122 <790490122@qq.com>
2019-01-29 16:45:53 +0800

    '活动优惠'

---
+++ application/admin/lang/zh-cn/active.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'Title'  =>  '标题',
+    'User_id'  =>  '发起用户',
+    'Desc'  =>  '活动内容',
+    'Coupon_name'  =>  '优惠券名称',
+    'Min_amount'  =>  '最低金额限度',
+    'Prefer_acount'  =>  '优惠金额',
+    'Start_time'  =>  '开始时间',
+    'End_time'  =>  '结束时间',
+    'Status'  =>  '状态',
+    'Status 0'  =>  '未通过',
+    'Status 1'  =>  '审核中',
+    'Status 2'  =>  '已通过',
+    'Weigh'  =>  '权重',
+    'Createtime'  =>  '创建时间',
+    'Updatetime'  =>  '更新时间'
+];
---
+++ application/admin/model/UserActive.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace app\admin\model;
+
+use think\Model;
+
+class UserActive extends Model
+{
+    // 表名
+    protected $name = 'user_active';
+    
+    // 自动写入时间戳字段
+    protected $autoWriteTimestamp = 'int';
+
+    // 定义时间戳字段名
+    protected $createTime = 'createtime';
+    protected $updateTime = 'updatetime';
+    
+    // 追加属性
+    protected $append = [
+        'start_time_text',
+        'end_time_text',
+        'status_text'
+    ];
+    
+
+    
+    public function getStatusList()
+    {
+        return ['0' => __('Status 0'),'1' => __('Status 1')];
+    }     
+
+
+    public function getStartTimeTextAttr($value, $data)
+    {
+        $value = $value ? $value : (isset($data['start_time']) ? $data['start_time'] : '');
+        return is_numeric($value) ? date("Y-m-d H:i:s", $value) : $value;
+    }
+
+
+    public function getEndTimeTextAttr($value, $data)
+    {
+        $value = $value ? $value : (isset($data['end_time']) ? $data['end_time'] : '');
+        return is_numeric($value) ? date("Y-m-d H:i:s", $value) : $value;
+    }
+
+
+    public function getStatusTextAttr($value, $data)
+    {        
+        $value = $value ? $value : (isset($data['status']) ? $data['status'] : '');
+        $list = $this->getStatusList();
+        return isset($list[$value]) ? $list[$value] : '';
+    }
+
+    protected function setStartTimeAttr($value)
+    {
+        return $value && !is_numeric($value) ? strtotime($value) : $value;
+    }
+
+    protected function setEndTimeAttr($value)
+    {
+        return $value && !is_numeric($value) ? strtotime($value) : $value;
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('User','user_id')->bind('username');
+    }
+
+
+}
